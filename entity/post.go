15c24fcdd228ddbb1e2266cbf1f439be06dc2b83
diyorbek <diyorbek5300@gmail.com>
2022-11-25 15:50:07 +0500

    test

---
+++ entity/post.go
@@ -0,0 +1,15 @@
+package entity
+
+import (
+	"time"
+)
+
+type Post struct {
+	ID          uint64 `gorm:"primary_key:auto_increment" json:"id"`
+	Title       string `gorm:"type: varchar(255)" json:"title"`
+	Description string `gorm:"type: text" json:"description"`
+	UserID      uint64 `gorm:"not null" json:"-"`
+	User        User   `gorm:"foreignkey:UserID;constraint:onUpdate:CASCADE,onDelete:CASCADE" json:"user"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
