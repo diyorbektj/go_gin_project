9bb89cce29632c065a9dadf5a0865e87a627d8ed
diyorbek <diyorbek5300@gmail.com>
2022-11-26 14:27:16 +0500

    test

--- controller/auth-controller.go
+++ controller/auth-controller.go
@@ -39,6 +39,7 @@ func NewAuthController(authService service.AuthService, jwtService service.JWTSe
 // @Param			password path		string			true	"Password"
 // @Success		200		{string}	string			"ok"
 // @Router			/auth/login [post]
+
 func (c *authController) Login(ctx *gin.Context) {
 	var loginDTO dto.LoginDTO
 	errDTO := ctx.ShouldBind(&loginDTO)
