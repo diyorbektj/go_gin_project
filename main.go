ec52dd418cdb38f9d8f816e74f5de3cd4ed81efc
diyorbektj <diyorbek5300@gmail.com>
2022-11-29 11:06:31 +0500

    test

--- main.go
+++ main.go
@@ -1,28 +1,13 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	swaggerfiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
 	"go_app/config"
-	"go_app/controller"
-	_ "go_app/controller"
-	docs "go_app/docs"
-	"go_app/middleware"
-	"go_app/repository"
-	"go_app/service"
 	"gorm.io/gorm"
+	"go_app/router"
 )
 
 var (
 	db             *gorm.DB                  = config.SetupDatabaseConnection()
-	userRepository repository.UserRepository = repository.NewUserRepository(db)
-	jwtService     service.JWTService        = service.NewJWTService()
-	userService    service.UserService       = service.NewUserService(userRepository)
-	authService    service.AuthService       = service.NewAuthService(userRepository)
-	authController controller.AuthController = controller.NewAuthController(authService, jwtService)
-	userController controller.UserController = controller.NewUserController(userService, jwtService)
-	homeController controller.HomeController = controller.NewHomeController()
 )
 
 // @title           Gin Book Service
@@ -34,23 +19,7 @@ var (
 
 func main() {
 	defer config.CloseDatabaseConnection(db)
-	r := gin.Default()
-	docs.SwaggerInfo.BasePath = "/api/"
-	r.Use(middleware.ApiCors())
-	r.GET("/", homeController.Home)
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
-	api := r.Group("api")
-	{
-		authRoute := api.Group("auth")
-		{
-			authRoute.POST("/login", authController.Login)
-			authRoute.POST("/register", authController.Register)
-		}
-		userRoute := api.Group("user").Use(middleware.AuthoizeJWT(jwtService))
-		{
-			userRoute.GET("/profile", userController.Profile)
-		}
-	}
+	r := router.setupRouter()
 	err := r.Run(":5500")
 	if err != nil {
 		return
---
+++ router/api.go
@@ -0,0 +1,48 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"go_app/config"
+	"go_app/controller"
+	_ "go_app/controller"
+	docs "go_app/docs"
+	"go_app/middleware"
+	"go_app/repository"
+	"go_app/service"
+	"gorm.io/gorm"
+)
+
+var (
+	db             *gorm.DB                  = config.SetupDatabaseConnection()
+	userRepository repository.UserRepository = repository.NewUserRepository(db)
+	jwtService     service.JWTService        = service.NewJWTService()
+	userService    service.UserService       = service.NewUserService(userRepository)
+	authService    service.AuthService       = service.NewAuthService(userRepository)
+	authController controller.AuthController = controller.NewAuthController(authService, jwtService)
+	userController controller.UserController = controller.NewUserController(userService, jwtService)
+	homeController controller.HomeController = controller.NewHomeController()
+)
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	docs.SwaggerInfo.BasePath = "/api/"
+	r.Use(middleware.ApiCors())
+	r.Use(gin.Logger())
+	r.GET("/", homeController.Home)
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+	api := r.Group("api")
+	{
+		authRoute := api.Group("auth")
+		{
+			authRoute.POST("/login", authController.Login)
+			authRoute.POST("/register", authController.Register)
+		}
+		userRoute := api.Group("user").Use(middleware.AuthoizeJWT(jwtService))
+		{
+			userRoute.GET("/profile", userController.Profile)
+		}
+	}
+	return r
+  }
